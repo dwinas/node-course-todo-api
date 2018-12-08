60f5b4fdac3a34db8ed70803bf166165da8486a1
Edvinas Petrauskas <dwinas@gmail.com>
2018-12-08 17:09:35 +0200

    Test POST / todos route

--- server/server.js
+++ server/server.js
@@ -1,40 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose.js')
-const {Todo} = require('./models/todo.js')
-const {user} = require('./models/user.js')
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-let app = express()
+var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/todos',(req, res) => {
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    const todo = new Todo({
-
-        text: req.body.text
-    })
-
-    todo.save().then((doc) => {
-
-        res.send(doc)
-
-    }), (e) => {
-
-        res.send(e)
-    }
-        console.log('Gautas reqeuestas')
-      
-})
-
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
 
-    console.log('Stared on port 3000')
-})
-
-
+module.exports = {app};
 
 
 
